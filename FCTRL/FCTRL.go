26c8f34cb8be97eeb097c8dfe92a04aeaaf1cc2d
LKK Pai <lkk@yahoo-inc.com>
2015-06-29 13:44:46 +0000

    Add FCTRL

---
+++ FCTRL/FCTRL.go
@@ -0,0 +1,21 @@
+package main
+import (
+    "fmt"
+)
+
+func Z(x int) int {
+    ret := 0
+    for i := 5; i <= x; i *= 5 {
+        ret += x / i
+    }
+    return ret
+}
+
+func main() {
+    var r, x int
+    fmt.Scan(&r)
+    for i := 1; i <= r; i++ {
+        fmt.Scan(&x)
+        fmt.Println(Z(x))
+    }
+}
